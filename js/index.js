e305583f3034670cb0211b2fa110c9e8f0083fd5
gitnubl33t <jessicad8695@gmail.com>
2020-08-11 09:22:21 -0700

    Added event to remove header image on click

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
-// Your code goes here
-
-//Nav Links - change text to @eastern-blue on click
+//Nav Links - change text to eastern-blue on mouseover, back to default on mouseout.
 
 const navLinks = document.querySelector(".nav");
 
-navLinks.addEventListener("click", (e) => {
+navLinks.addEventListener("mouseover", (e) => {
   e.target.style.color = "#17A2B8";
 });
 
 navLinks.addEventListener("mouseout", (e) => {
   e.target.style.color = "#212529";
+});
+
+//Remove header image on click
+
+const funBus = document.querySelector("img");
+
+funBus.addEventListener("click", (e) => {
+  e.target.style.display = "none";
 });
