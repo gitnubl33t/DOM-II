63310188d830698ac4ef4b89df57a9ddfa012812
gitnubl33t <jessicad8695@gmail.com>
2020-08-11 08:53:27 -0700

    Added 1st event, change color on navlinks on click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+//Nav Links - change text to @eastern-blue on click
+
+const navLinks = document.querySelector(".nav");
+
+navLinks.addEventListener("click", (e) => {
+  e.target.style.color = "#17A2B8";
+});
